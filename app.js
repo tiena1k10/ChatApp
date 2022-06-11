0be198374081d499fcf7189ae412d4cf900c4ab8
tiena1k10 <tiena1k10@gmail.com>
2022-06-11 23:12:56 +0700

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser')
 const socketio = require("socket.io");
 
 
-
+// @@
 
 const app = express();
 
@@ -31,16 +31,16 @@ const PORT = process.env.PORT || 3000;
 
 // database connection
 const dbURI = 'mongodb+srv://tiena1k10:123456T@chatapp.do7ro.mongodb.net/ChatApp?retryWrites=true&w=majority';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then((result) => {
     var server = app.listen(PORT)
     var io = socketio(server)
     const socketioControl = require("./controllers/socketio");
     socketioControl(io);
     console.log("Express server listening on port %d in %s mode", PORT, app.settings.env);
-    
 
-    })
+
+  })
   .catch((err) => console.log(err));
 
 // routes
